b6d315b212742c84d0afad9135979f516b922196
sungsujang99 <53261699+sungsujang99@users.noreply.github.com>
2025-06-20 14:20:09 +0900

    multi_camera

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import { Camera } from "./screens/Camera";
 import { DashboardOn } from "./screens/DashboardOn";
 import { DivWrapper } from "./screens/DivWrapper";
@@ -8,42 +8,53 @@ import { LoginScreen } from "./screens/LoginScreen";
 import { WorkStatus } from "./screens/WorkStatus";
 import SessionMonitor from "./components/SessionMonitor";
 
+// Layout component that includes SessionMonitor
+const Layout = () => {
+  return (
+    <>
+      <SessionMonitor />
+      <Outlet />
+    </>
+  );
+};
+
 const router = createBrowserRouter([
   {
-    path: "/*",
-    element: <Login />,
-  },
-  {
-    path: "/login1u40u4363u4449u4363u4469u4355u4469-u4363u4469u4536u4357u4455u4520u41",
-    element: <Login />,
-  },
-  {
-    path: "/dashboardu40onu41",
-    element: <DashboardOn />,
-  },
-  {
-    path: "/login2u40u4359u4469u4358u4469u4527u4359u4453u4523u4370u4457-u4363u4469u4536u4357u4455u4520u41",
-    element: <LoginScreen />,
-  },
-  {
-    path: "/login1u40u4363u4449u4363u4469u4355u4469-u4363u4469u4536u4357u4455u4520u41-u4363u4454u4357u4453",
-    element: <DivWrapper />,
-  },
-  {
-    path: "/camera-1",
-    element: <Camera />,
-  },
-  {
-    path: "/work-status",
-    element: <WorkStatus />,
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/*",
+        element: <Login />,
+      },
+      {
+        path: "/login1u40u4363u4449u4363u4469u4355u4469-u4363u4469u4536u4357u4455u4520u41",
+        element: <Login />,
+      },
+      {
+        path: "/dashboardu40onu41",
+        element: <DashboardOn />,
+      },
+      {
+        path: "/login2u40u4359u4469u4358u4469u4527u4359u4453u4523u4370u4457-u4363u4469u4536u4357u4455u4520u41",
+        element: <LoginScreen />,
+      },
+      {
+        path: "/login1u40u4363u4449u4363u4469u4355u4469-u4363u4469u4536u4357u4455u4520u41-u4363u4454u4357u4453",
+        element: <DivWrapper />,
+      },
+      {
+        path: "/camera-1",
+        element: <Camera />,
+      },
+      {
+        path: "/work-status",
+        element: <WorkStatus />,
+      },
+    ]
   },
 ]);
 
 export const App = () => {
-  return (
-    <>
-      <SessionMonitor />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
